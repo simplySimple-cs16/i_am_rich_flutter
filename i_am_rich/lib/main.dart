6a07ea8771bc582b05713fbac37ae6a8d748d8b2
Kadum Perme <kadumperme@Kadums-MacBook-Pro.local>
2020-04-17 05:29:25 +0530

    First Update

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('Iam Rich'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image.asset('images/diamond.png'),
+          ),
+        ),
+      ),
+    );
